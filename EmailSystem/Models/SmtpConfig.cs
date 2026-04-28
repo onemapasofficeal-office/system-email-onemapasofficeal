3a94c84c7d4ef8b9f508f96d19ed15a9414dee3b
onemapasofficeal-off <onemapasofficeal.jjk@gmail.com>
2026-04-28 19:21:02 -0300

    chore: upload SmtpConfig.cs

---
+++ EmailSystem/Models/SmtpConfig.cs
@@ -0,0 +1,10 @@
+namespace EmailSystem.Models;
+
+public class SmtpConfig
+{
+    public string Host     { get; set; } = string.Empty;
+    public int    Port     { get; set; } = 587;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool   UseSsl   { get; set; } = true;
+}
